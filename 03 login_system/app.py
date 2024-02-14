7a619ca98214d701d48fc90a8113970c5ec05fc0
thasvithu <thasvithu7@gmail.com>
2024-02-14 22:41:55 +0530

    project 3 added

---
+++ 03 login_system/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return  render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-projects
+
+## 01 Python MySQL Connection
+## 02 CRUD Application using Python, SQL And Flask
+## 03 login_system
